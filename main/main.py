79c9a44264bbd3bd913fda25d5767752ee58f059
Bryan Yi <bryanyi1206@gmail.com>
2021-03-22 10:50:00 -0700

    dotenv bug

--- main/main.py
+++ main/main.py
@@ -1,62 +1,38 @@
-from dataclasses import dataclass
-from flask import Flask, jsonify, abort
-from flask_cors import CORS
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
-import requests
-
-from producer import publish
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+# CONNECTING TO DATABASE
+app.config["SQLAlchemy_DATABASE_URI"] = os.getenv('SQLALCHEMY_URI')
 CORS(app)
 
 db = SQLAlchemy(app)
 
-
-@dataclass
 class Product(db.Model):
-    id: int
-    title: str
-    image: str
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
-
-
-@dataclass
+    description = db.Column(db.String(1000))
+    
 class ProductUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id=db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
-
+    
+    # COMBINATION OF USER_ID AND PRODUCT_ID SHOULD STAY UNIQUE & NOT REPEATED
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+    
+    
 
-
-@app.route('/api/products')
+@app.route('/')
 def index():
-    return jsonify(Product.query.all())
-
-
-@app.route('/api/products/<int:id>/like', methods=['POST'])
-def like(id):
-    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
-    json = req.json()
-
-    try:
-        productUser = ProductUser(user_id=json['id'], product_id=id)
-        db.session.add(productUser)
-        db.session.commit()
-
-        publish('product_liked', id)
-    except:
-        abort(400, 'You already liked this product')
-
-    return jsonify({
-        'message': 'success'
-    })
-
+    return 'Hellooooergergergergsergse!!!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
