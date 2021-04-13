a3f72511a0d78e72b27c7a78dcb3ff7bc5e38d08
Bryan Yi <bryanyi1206@gmail.com>
2021-04-12 22:40:01 -0700

    creating CRUD operations

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -1,4 +1,7 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't_pij+hk^h@3gc8sy^trk)yc7j*=ntlvn)wh42c-ll!&rqt%11'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
