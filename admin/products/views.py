f79c9ff6c1d128d1e6d08847747f289435d00d56
Bryan Yi <bryanyi1206@gmail.com>
2021-04-18 14:13:36 -0700

    CRUD operations

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import Product
+from rest_framework.views import APIView
+from .models import Product, User
 from .serializers import ProductSerializer
+import random
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
@@ -18,7 +20,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    def retreive(self, request, pk=None):
+    def retrieve(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
@@ -31,6 +33,15 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
     
     def destroy(self, request, pk=None):
-        products = Product.objects.get(id=pk)
+        product = Product.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id':user.id
+        })
+        
