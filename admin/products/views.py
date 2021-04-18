8316ec70914932f03b93d5bbf632465df7abeadd
Bryan Yi <bryanyi1206@gmail.com>
2021-04-17 18:38:20 -0700

    CRUD operations

--- admin/products/views.py
+++ admin/products/views.py
@@ -31,4 +31,6 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
     
     def destroy(self, request, pk=None):
-        pass
+        products = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
