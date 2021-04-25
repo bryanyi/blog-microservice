2137f7ea4d00798f56ab0d5124a2a9dabcf9113b
Bryan Yi <bryanyi1206@gmail.com>
2021-04-24 21:10:35 -0700

    creating Flask app

--- main/main.py
+++ main/main.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 app.config("SQLALCHEMY_DATABASE_URI") = os.getenv("SQL_ALCHEMY")
 CORS(app)
 
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+
 @app.route('/')
 def index():
     return 'hello from flask! =)'
