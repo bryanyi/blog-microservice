d2a38693beef63d8e0fe42cfd8428e95116c831d
Bryan Yi <bryanyi1206@gmail.com>
2021-05-04 09:06:18 -0700

    creating Flask app & connecting it do a database

--- main/main.py
+++ main/main.py
@@ -15,6 +15,10 @@ db = SQLAlchemy(app)
 
 @dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
