c8fb07cc8ad2549d3ca22ecd2d1c7c0da6c72fcd
Bryan Yi <bryanyi1206@gmail.com>
2021-05-08 09:48:58 -0700

    connecting to rabbitMQ

---
+++ admin/consumer.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import pika
+
+url = os.getenv("AMQPS_KEY")
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
